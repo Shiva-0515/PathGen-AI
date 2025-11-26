94c28f46a521cf5f789bd3d0d4a38b9dad306266
Shiva-0515 <shivakanchanapelli24@gmail.com>
2025-11-26 15:35:40 +0530

    minor changes@1.0.2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,21 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { ClerkProvider } from "@clerk/clerk-react";
-// import ClerkAppearanceProvider from "./components/ClerkAppearanceProvider";
 
-const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Clerk Publishable Key");
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <ClerkAppearanceProvider publishableKey={PUBLISHABLE_KEY}> */}
-        <App />
-      {/* </ClerkAppearanceProvider> */}
+     <App />
   </React.StrictMode>
 );
 
